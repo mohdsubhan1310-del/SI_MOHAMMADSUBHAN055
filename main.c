f4510cdb69ce46f00d54e66dd726596ce8381dd4
mohdsubhan1310-del <mohdsubhan1310@gmail.com>
2026-07-06 16:30:08 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+   int number=153,count=0,rem,sum=0,power=1;
+   for(int temp=number;temp>0;temp=temp/10)
+   {
+       power=1;
+       rem=temp1%10;
+  for(int 1=1;i<=count;i++)
+  {
+      power*rem;
+  }
+  sum=sum+power;
+   }
+      if(sum==n)
+      {
+          printf("Number %d is an Armstrong Number ")
+      }
+return 0;
+
+
+
+
+
